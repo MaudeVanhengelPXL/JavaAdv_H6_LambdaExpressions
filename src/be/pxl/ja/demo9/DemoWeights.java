b23c69684c2d802ba0d2dd234958a314de932a76
MaudeVanhengelPXL <maude.vanhengel@student.pxl.be>
2020-10-30 10:51:13 +0100

    DemoWeights korter geschreven

--- src/be/pxl/ja/demo9/DemoWeights.java
+++ src/be/pxl/ja/demo9/DemoWeights.java
@@ -5,12 +5,14 @@ public class DemoWeights {
 	public static void main(String[] args) {
 		Weights weights = new Weights();
 		System.out.println("Rounding down");
-		weights.roundWeights(d -> Double.valueOf(Math.floor(d)).intValue());
+		//weights.roundWeights(d -> Double.valueOf(Math.floor(d)).intValue());
+		weights.roundWeights(d -> (int) Math.floor(d));
 
 		System.out.println("Rounding up");
-		weights.roundWeights(d -> Double.valueOf(Math.ceil(d)).intValue());
+		//weights.roundWeights(d -> Double.valueOf(Math.ceil(d)).intValue());
+		weights.roundWeights(d -> (int) Math.ceil(d));
 
 		System.out.println("Displaying");
-		weights.consumeWeights(d -> System.out.println(String.format("%.2f", d)));
+		weights.consumeWeights(d -> System.out.printf("%.2f%n", d));
 	}
 }
